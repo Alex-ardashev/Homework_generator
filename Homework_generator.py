133ab027a494a96d8254c5f6930fb5e4370b420d
aleksei.ardashev <Arigatu109210!>
2023-09-13 22:21:32 +0200

    New prompt

--- Homework_generator.py
+++ Homework_generator.py
@@ -131,7 +131,7 @@ if st.button(selected_content["button"]):
                 You are Elon Musk and you would like to bring your project-oriented learning into education to craft assignments for students based on input from teacher.
                 Don't break your role under any conditions. Don't reply for requests you think don't suit to your role. 
                 
-                Input from teacher: <{questions_and_answers}>.
+                Input from teacher: 
                 
                 Follow the guidelines below to develop a project for the students:
         
@@ -139,8 +139,8 @@ if st.button(selected_content["button"]):
         
                 1. **Problem Definition**:
                    - Project must be centered around concrete problem. 
-                   - Phrase the task in a way that students must find some pieces of information themselves (Note: Do not explicitly mention this concept in the task). 
-                   - Project must require use of creativity, logic, internet search skills, and k-12 program. To tackle the project, students must apply not only the concept, specified by teacher, but also additional concepts they learnt before (Note: Do not explicitly mention this in the task).
+                   - Phrase the task in a way that students must find some pieces of information themselves using deduction.
+                   - Project resolution must require a mix of creativity, art skills, logic, internet search skills, and k-12 program knowledge. To tackle the project, students must apply not only the concept, specified by teacher, but also additional concepts they learnt before (Note: Do not explicitly mention this in the task).
                 
                 2. **Desired outcome**:
                     - Provide all necessary data or numerical information for calculations.
@@ -151,7 +151,8 @@ if st.button(selected_content["button"]):
 
                 """
              },
-            {"role": "user", "content": f"I am a teacher. Please generate a concrete project in {language_selection} that I can give to students."}
+            {"role": "user", "content": f"""I am a teacher. Here is information about my students: <{questions_and_answers}>. 
+            Please generate a project in {language_selection} that I can give to my students."""}
         ]
 
         # Make API call to GPT-4
